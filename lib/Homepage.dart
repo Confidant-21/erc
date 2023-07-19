b06341af442342fa0aebaead7bb89533a4950a72
Swastik Gupta <swastik@google.com>
2023-07-19 21:08:11 +0530

    Color theme Exchanged

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -33,25 +33,26 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFF1F1E6),
         body: _children[_selectIndex],
         bottomNavigationBar: Container(
-          color: Color(0xFF04356D),
+          color: Color(0xFFF1F1E6),
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 16),
             child: GNav(
-              backgroundColor: Color(0xFF04356D),
-              activeColor: Color(0xFF04356D),
-              tabBackgroundColor: Color(0xFFF1F1E6),
+              backgroundColor: Color(0xFFF1F1E6),
+              activeColor: Color(0xFFF1F1E6),
+              tabBackgroundColor: Color(0xFF04356D),
               padding: EdgeInsets.all(16),
               gap: 8,
               onTabChange: _navigationBottomBar,
               selectedIndex: _selectIndex,
               tabs: const [
-                GButton(icon: Icons.home_filled,iconColor: Color(0xFFF1F1E6),text: 'Home', ),
-                GButton(icon: Icons.event_note,iconColor: Color(0xFFF1F1E6),text: 'Log', ),
-                GButton(icon: Icons.group_add,iconColor: Color(0xFFF1F1E6), text: 'Tally'),
-                GButton(icon: Icons.group,iconColor: Color(0xFFF1F1E6),text: 'Team'),
-                GButton(icon: Icons.message_outlined, iconColor:Color(0xFFF1F1E6),text: 'Chats'),
+                GButton(icon: Icons.home_filled,iconColor: Color(0xFF04356D),text: 'Home', ),
+                GButton(icon: Icons.event_note,iconColor: Color(0xFF04356D),text: 'Log', ),
+                GButton(icon: Icons.group_add,iconColor: Color(0xFF04356D), text: 'Tally'),
+                GButton(icon: Icons.group,iconColor: Color(0xFF04356D),text: 'Team'),
+                GButton(icon: Icons.message_outlined, iconColor:Color(0xFF04356D),text: 'Chats'),
               ],
             ),
           ),
--- lib/PagesHome/Home.dart
+++ lib/PagesHome/Home.dart
@@ -11,7 +11,9 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor:Colors.transparent,// Color(0xFFF1F1E6),
+        backgroundColor: Colors.transparent, // Color(0xFFF1F1E6),
+        // floatingActionButton: FloatingActionButton.extended(
+        //     onPressed: () {}, label: Icon(Icons.add_photo_alternate_outlined,color: Color(0xFF04356D),weight: double.maxFinite),backgroundColor: Colors.transparent),
         body: CustomScrollView(
           slivers: [
             SliverAppBar(
@@ -22,20 +24,26 @@ class _HomeState extends State<Home> {
                     onPressed: () {},
                     icon: Icon(Icons.add_photo_alternate_rounded),
                     style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20)))),
+                        backgroundColor: Colors.transparent,
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20)))),
                     label: Text('Add Post'),
                   ),
                   ElevatedButton.icon(
                     onPressed: () {},
                     icon: Icon(Icons.settings_accessibility_outlined),
                     style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20)))),
+                        backgroundColor: Colors.transparent,
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20)))),
                     label: Text('Settings'),
                   )
                 ],
               ),
-              expandedHeight: 400,
-              backgroundColor: Color(0xFF04356D),
+              expandedHeight: (MediaQuery.of(context).size.height) * 0.5,
+              backgroundColor: Color(0xFFF1F1E6),
               pinned: true,
               flexibleSpace: FlexibleSpaceBar(
                 background: Image.asset("assets/Images/Feed/homemain.png",
@@ -48,7 +56,7 @@ class _HomeState extends State<Home> {
                 child: Container(
                   // height: MediaQuery.of(context).size.height,
                   // width: MediaQuery.of(context).size.width,
-                  color: Color(0xFF04356D),
+                  color: Color(0xFFF1F1E6),
                 ),
               ),
             ),
@@ -58,7 +66,7 @@ class _HomeState extends State<Home> {
                 child: Container(
                   height: 500,
                   width: 300,
-                  color: Color(0xFF04356D),
+                  color: Color(0xFFF1F1E6),
                 ),
               ),
             ),
